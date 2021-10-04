b8e5e4e2028e9aacd812a28d0afdd7da33f65240
RajendraGund <rajendragund2015@gmail.com>
2021-10-04 17:56:25 +0530

    Added : Program for the Create Contact in Address book system

--- src/main/java/com/bridgelabz/workshop/AddressBookMain.java
+++ src/main/java/com/bridgelabz/workshop/AddressBookMain.java
@@ -3,5 +3,8 @@ package com.bridgelabz.workshop;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System");
+        Contacts contact = new Contacts("Rajendra", "Gund", "Shirdi", "Ahmednagar", "Maharashtra", 414501, "9604315270", "rajendragund@gmail.com");
+        System.out.println(contact);
     }
+
 }
