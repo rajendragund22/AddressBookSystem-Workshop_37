d4f6d38bd75cd867a172144a805fe596fee83a1a
RajendraGund <rajendragund2015@gmail.com>
2021-10-05 05:16:59 +0530

    Added : Program for the search person by city into address book system

--- src/main/java/com/bridgelabz/workshop/AddressBook.java
+++ src/main/java/com/bridgelabz/workshop/AddressBook.java
@@ -5,12 +5,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class AddressBook {
+public class AddressBook
+{
     static Scanner scanner = new Scanner(System.in);
     ArrayList<Contact> contactlist = new ArrayList<>();
-    private Map<String, ArrayList<Contact>> AddressBookMain = new HashMap<>();
+    private Map<String, ArrayList<Contact>> addressbook = new HashMap<>();
+    private Object contact;
 
-    public void addNewContact() {
+    public void addNewContact()
+    {
         Contact contact = new Contact();
 
         System.out.println("Enter First name:");
@@ -38,33 +41,38 @@ public class AddressBook {
         contact.setEmailid(scanner.next());
 
         System.out.println("Enter Book name to which you have to add contact");
-        String bookName = scanner.next();
-        if (AddressBookMain.containsKey(bookName)) {
-            contactlist.stream().filter(value -> value.getFirstname().equals(contact.getFirstname())).forEach(value ->
+        String bookName  = scanner.next();
+        if(addressbook.containsKey(bookName))
+        {
+            contactlist.stream().filter(value -> value.getFirstname(). equals(contact.getFirstname())).forEach(value ->
             {
                 System.out.println("Duplicate Contact");
                 addNewContact();
             });
             contactlist.add(contact);
-            AddressBookMain.put(bookName, contactlist);
+            addressbook.put(bookName,contactlist);
             System.out.println("New Contact Has Been Added Successfully");
-        } else {
+        }
+        else
+        {
             contactlist.add(contact);
-            AddressBookMain.put(bookName, contactlist);
+            addressbook.put(bookName,contactlist);
             System.out.println("New AddressBook is created and Added Contact in the AddressBook Successfully");
         }
     }
-
-    public void editContact() //edit contact.
+    public void editContact()
     {
         String enteredFirstName;
         System.out.println("Enter First name of contact to edit it ");
         enteredFirstName = scanner.next();
-        for (int i = 0; i < contactlist.size(); i++) {
-            if (contactlist.get(i).getFirstname().equals(enteredFirstName)) {
+        for (int i = 0; i < contactlist.size(); i++)
+        {
+            if (contactlist.get(i).getFirstname().equals(enteredFirstName))
+            {
                 System.out.println("Enter the field to edit:\n1.First Name\n2.Last Name\n3.Address\n4.city\n5.State\n6.Zip\n7.Phone\n8.Email");
                 int userInput = scanner.nextInt();
-                switch (userInput) {
+                switch (userInput)
+                {
                     case 1:
                         System.out.println("Enter new first name");
                         contactlist.get(i).setFirstname(scanner.next());
@@ -106,9 +114,12 @@ public class AddressBook {
         System.out.println("Contact Edited Successfully");
     }
 
-    public void deleteContact(String name) {
-        for (int i = 0; i < contactlist.size(); i++) {
-            if (contactlist.get(i).getFirstname().equals(name)) {
+    public void deleteContact(String name)
+    {
+        for (int i = 0; i < contactlist.size(); i++)
+        {
+            if (contactlist.get(i).getFirstname().equals(name))
+            {
                 Contact contact = contactlist.get(i);
                 contactlist.remove(contact);
             }
@@ -116,7 +127,17 @@ public class AddressBook {
         System.out.println("Contact Deleted Successfully");
     }
 
-    public void displayList() {
+    public void searchaPersoninaCity (String city)
+    {
+        System.out.println("following are the persons who belongs to :" + city);
+        for(String bookName : addressbook.keySet())
+        {
+            addressbook.get(bookName);
+            contactlist.stream().filter(value -> value.getCity().equals(city)).map(Contact::getFirstname).forEach(System.out::println);
+        }
+    }
+    public void displayList()
+    {
         for (Contact iterator : contactlist) System.out.println(iterator);
     }
 
--- src/main/java/com/bridgelabz/workshop/AddressBookMain.java
+++ src/main/java/com/bridgelabz/workshop/AddressBookMain.java
@@ -1,6 +1,5 @@
 package com.bridgelabz.workshop;
 
-
 import java.util.Scanner;
 
 public class AddressBookMain {
@@ -10,7 +9,7 @@ public class AddressBookMain {
         boolean isExit = false;
         while (!isExit) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter options \n 1.Add Contact.\n 2.Edit Contact.\n 3.Delete Contact.\n 4.Display Contact. \n 5. Exit");
+            System.out.println("Enter options \n 1.Add Contact.\n 2.Edit Contact.\n 3.Delete Contact.\n 4. Search City.\n 5.Display Contact. \n 6. Exit");
             int userInput = scanner.nextInt();
             switch (userInput) {
                 case 1:
@@ -23,11 +22,14 @@ public class AddressBookMain {
                     System.out.println("Enter the Name of the Person do you Wants to Delete:");
                     String personName = scanner.next();
                     service.deleteContact(personName);
-
                 case 4:
+                    System.out.println("Enter the city to be Searched:");
+                    String searchCity = scanner.next();
+                    service.searchaPersoninaCity(searchCity);
+                case 5:
                     service.displayList();
                     break;
-                case 5:
+                case 6:
                     isExit = true;
                     break;
                 default:
