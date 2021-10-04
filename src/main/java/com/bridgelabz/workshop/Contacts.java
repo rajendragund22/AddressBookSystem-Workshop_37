fbb674d7af129199cb69b834a673fe8a5aad5729
RajendraGund <rajendragund2015@gmail.com>
2021-10-05 04:33:27 +0530

    Added : Program for the Edit existing contact in the Address book

--- src/main/java/com/bridgelabz/workshop/Contacts.java
+++ src/main/java/com/bridgelabz/workshop/Contacts.java
@@ -1,110 +1,83 @@
 package com.bridgelabz.workshop;
 
-public class Contacts {
+class Contact {
     private String firstName;
     private String lastName;
     private String address;
     private String city;
     private String state;
-    private long zip;
-    private String PhoneNumber;
-    private String emailId;
-
-    public Contacts(String firstName, String lastName, String address, String city, String state, long zip, String PhoneNumber, String emailId) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.PhoneNumber = PhoneNumber;
-        this.emailId = emailId;
-    }
-
+    private String email;
+    private String zip;
+    private String phoneNumber;
 
     public String getFirstName() {
         return firstName;
     }
 
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
 
     public String getLastName() {
         return lastName;
     }
 
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
 
     public String getAddress() {
         return address;
     }
 
-
     public void setAddress(String address) {
         this.address = address;
     }
-
 
     public String getCity() {
         return city;
     }
 
-
     public void setCity(String city) {
         this.city = city;
     }
-
 
     public String getState() {
         return state;
     }
 
-
     public void setState(String state) {
         this.state = state;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
-    public long getZip() {
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getZip() {
         return zip;
     }
 
-
-    public void setZip(int zip) {
+    public void setZip(String zip) {
         this.zip = zip;
     }
 
-
     public String getPhoneNumber() {
-        return PhoneNumber;
+        return phoneNumber;
     }
-
 
     public void setPhoneNumber(String phoneNumber) {
-        PhoneNumber = phoneNumber;
+        this.phoneNumber = phoneNumber;
     }
-
-
-    public String getEmailId() {
-        return emailId;
-    }
-
-
-    public void setEmailId(String emailId) {
-        this.emailId = emailId;
-    }
-
 
     @Override
     public String toString() {
-        return "Contacts [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
-                + ", state=" + state + ", zip=" + zip + ", PhoneNumber=" + PhoneNumber + ", emailId=" + emailId + "]";
+        return "ContactItems [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
+                + city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email
+                + "]";
     }
 }
