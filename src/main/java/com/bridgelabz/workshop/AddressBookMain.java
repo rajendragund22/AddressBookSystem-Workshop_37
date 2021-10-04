bcf1489f8221d430749e75ba1e6fc62dba2ba67a
RajendraGund <rajendragund2015@gmail.com>
2021-10-04 21:06:19 +0530

    Added : Program for the Add new contact in the address book system

--- src/main/java/com/bridgelabz/workshop/AddressBookMain.java
+++ src/main/java/com/bridgelabz/workshop/AddressBookMain.java
@@ -1,10 +1,41 @@
 package com.bridgelabz.workshop;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book System");
-        Contacts contact = new Contacts("Rajendra", "Gund", "Shirdi", "Ahmednagar", "Maharashtra", 414501, "9604315270", "rajendragund@gmail.com");
-        System.out.println(contact);
+    Contacts contacts;
+
+    private void addContacts() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter The First Name  :- ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter The Last Name :- ");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter The Address :- ");
+        String address = scanner.nextLine();
+        System.out.println("Enter The City :- ");
+        String city = scanner.nextLine();
+        System.out.println("Enter The State :- ");
+        String state = scanner.nextLine();
+        System.out.println("Enter The Zip Code :- ");
+        int zipCode = scanner.nextInt();
+        System.out.println("Enter The Mobile Number :- ");
+        String number = scanner.next();
+        System.out.println("Enter The Email-id :- ");
+        String email = scanner.next();
+        contacts = new Contacts(firstName, lastName, address, state, city, zipCode, number, email);
+        System.out.println("Contact Successfully Added");
     }
 
+    @Override
+    public String toString() {
+        return contacts.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book System");
+        AddressBookMain addressBook = new AddressBookMain();
+        addressBook.addContacts();
+        System.out.println(addressBook.toString());
+    }
 }
